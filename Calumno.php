7f89780be0e90a5f2c76a3711178ab371da46029
osmar <osmarernesto17@gmail.com>
2023-04-19 13:44:00 -0600

    init

---
+++ Calumno.php
@@ -0,0 +1,34 @@
+<?php
+class Alumno {
+    public $correo;
+    public $nombre;
+    public $carnet;
+    public $edad;
+    public $curso;
+    public $foto;
+
+    public function __construct($arg_correo="", $arg_nombre="", $arg_carnet="", $arg_edad="",$arg_curso="", $arg_foto="")
+    {
+        $this->correo=$arg_correo;
+        $this->nombre=$arg_nombre;
+        $this->carnet=$arg_carnet;
+        $this->edad=$arg_edad;
+        $this->curso=$arg_curso;
+        $this->foto=$arg_foto;
+    }
+
+    public function imprimir()
+    {
+        echo "Correo Electrónico: $this->correo <br/>";
+        echo "Nombre: $this->nombre <br/>";
+        echo "Número de Carnet: $this->carnet <br/>";
+        echo "Edad: $this->edad <br/>";
+        echo "Curso Actual: $this->curso <br/>";
+        echo "Foto: $this->foto <br/>";
+    }
+    
+    function __destruct() {
+    
+    }
+}
+?>
