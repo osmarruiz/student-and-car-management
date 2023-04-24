401cae8b9ec147539f5f187608f749085f358ff3
osmar <osmarernesto17@gmail.com>
2023-04-24 07:44:37 -0600

    feat: new function for procesar

--- procesar.php
+++ procesar.php
@@ -1,16 +1,26 @@
 <?php
+echo"<link rel='stylesheet' href='css/bootstrap.min.css'>";
 include_once("Calumno.php");
-include_once("serializar.php");
+include_once("Cserializar.php");
 
 if(isset($_REQUEST['procesar']))
 {
-    $correo = $_REQUEST['correo'];
-    $nombre = $_REQUEST['nombre'];
-    $numero = $_REQUEST['numero'];
-    $edad = $_REQUEST['edad'];
-    $curso = $_REQUEST['curso'];
-    $imagen = $_FILES['imagen'];
-    
+
+    $listalumno = Cserializar::deserializar();
+    $newalumno = new Alumno($_POST['correo'], $_POST['nombre'], $_POST['numero'], $_POST['edad'], $_POST['curso'], $_FILES['imagen']);
+    $newalumno->guardarImagen();
+    array_push($listalumno, $newalumno);
+    $result = Cserializar::serializar($listalumno);
+
+    if($result)
+    {
+        echo "<div class='bg-success'><h3 class='text-white text-center pt-4 pb-4'>Datos insertado correctamente</h3></div>";
+    }
+    else
+    {
+        echo "<div class='bg-danger'><h3 class='text-white text-center pt-4 pb-4'>ocurrio un error</h3></div>";
+    }
+    echo "<a href='index.html'>Regresar a la pagina principal</a>";
 }
 else
 echo "primera visita";
